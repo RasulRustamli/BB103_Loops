32d862db635df61652f3303d1d2da4d86022153b
RasulRustamli <rasular@code.edu.az>
2023-09-27 09:38:48 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,96 @@
+﻿//int point = 77;
+
+//if(point>=0 && point<=100)
+//{
+//    if(point>=91)
+//    {
+//        Console.WriteLine("A almisan");
+//    }
+//    else if(point>=81)
+//    {
+//        Console.WriteLine("B almisan");
+//    }
+//    else if(point>=71)
+//    {
+//        Console.WriteLine("C almisan");
+//    }
+//    else if(point >=61)
+//    {
+//        Console.WriteLine("D almisan");
+//    }
+//    else if(point>=51)
+//    {
+//        Console.WriteLine("E almisan");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Kesilmisen");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Bele bir point yoxdur");
+//}
+
+
+
+
+
+//int i = 1;
+//int sum = 0;
+
+//while(i<=10)
+//{
+//    if(i%2==0)
+//    {
+//        sum += i;
+//    }
+//    i++;
+
+
+//}
+//Console.WriteLine("cemi " + sum);
+
+
+int i = 1;
+int sum = 0;
+
+while (i <= 100)
+{
+    if (i % 5 == 0)
+    {
+        sum += i;
+    }
+    i++;
+}
+Console.WriteLine("merhele sayi" + i);
+
+//var sum = 0;
+//for (int i = 1; i <= 100; i++)
+//{
+//    if (i % 5 == 0)
+//    {
+//        sum += i;
+//    }
+
+//}
+//Console.WriteLine(sum);
+
+//int i = 101;
+//int sum = 0;
+
+//while (i <= 100)
+//{
+//    if (i % 5 == 0)
+//    {
+//        sum += i;
+//    }
+//    i++;
+//    Console.WriteLine("salam");
+//}
+//do
+//{
+//    Console.WriteLine("sagolun");
+//} while (i <= 100);
+
+
